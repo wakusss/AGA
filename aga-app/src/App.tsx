47de7e8cb7a6dfc01a09f1bf69cb9dab19ce3107
Wakus <gerega.2007@gmail.com>
2026-02-10 14:36:42 +0100

    added new components and dependencies

--- aga-app/src/App.tsx
+++ aga-app/src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from "./components/login/Login.tsx";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <p>Aga app</p>
+       <Login />
     </>
   )
 }
