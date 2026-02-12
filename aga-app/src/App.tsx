c29f0020a060c45b500008e2445c186a3eeca193
Wakus <gerega.2007@gmail.com>
2026-02-12 13:09:39 +0100

    created nav-bar folder

--- aga-app/src/App.tsx
+++ aga-app/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavBar from "./components/nav-bar/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
