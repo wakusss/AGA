6e746e4041925cc7112f5df659ecd29fdf00b82f
Wakus <gerega.2007@gmail.com>
2026-02-12 15:07:17 +0100

    added new styles to nav-bar

--- aga-app/src/components/nav-bar/NavBar.tsx
+++ aga-app/src/components/nav-bar/NavBar.tsx
@@ -3,20 +3,20 @@ import React from "react";
 export default function NavBar() {
   return (
     <>
-      <nav className="navbar grid grid-cols-2 text-purple-700">
+      <nav className="navbar grid grid-cols-2 bg-purple-700 text-white p-7">
         <div className="navbar-logo">
-          <h1 className="text-5xl pl-8">
+          <h1 className="text-5xl">
             <a href="/feed">AGA Chat</a>
           </h1>
         </div>
-        <ul className="flex space-x-4 grid grid-cols-3">
-          <li className="text-xl font-bold text-center">
+        <ul className="grid grid-cols-3">
+          <li className="text-xl text-center flex items-center">
             <a href="/feed">Feed</a>
           </li>
-          <li className="text-xl font-bold text-center">
+          <li className="text-xl text-center flex items-center">
             <a href="/profile">Profile</a>
           </li>
-          <li className="text-xl font-bold text-center">
+          <li className="text-xl text-center flex items-center">
             <a href="/settings">Settings</a>
           </li>
         </ul>
