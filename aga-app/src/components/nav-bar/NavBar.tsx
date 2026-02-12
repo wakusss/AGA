ed6b357e3cff168531c6f3cf9050431e6f3553a7
Wakus <gerega.2007@gmail.com>
2026-02-12 14:55:15 +0100

    added new font and changed nav-bar

--- aga-app/src/components/nav-bar/NavBar.tsx
+++ aga-app/src/components/nav-bar/NavBar.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function NavBar() {
-  return <div>Nav-Bar</div>;
+  return (
+    <>
+      <nav className="navbar grid grid-cols-2 text-purple-700">
+        <div className="navbar-logo">
+          <h1 className="text-5xl pl-8">
+            <a href="/feed">AGA Chat</a>
+          </h1>
+        </div>
+        <ul className="flex space-x-4 grid grid-cols-3">
+          <li className="text-xl font-bold text-center">
+            <a href="/feed">Feed</a>
+          </li>
+          <li className="text-xl font-bold text-center">
+            <a href="/profile">Profile</a>
+          </li>
+          <li className="text-xl font-bold text-center">
+            <a href="/settings">Settings</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
