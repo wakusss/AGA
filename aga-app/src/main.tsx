9b469f760954f4ff92474218845d03c7e4fec5e3
Wakus <gerega.2007@gmail.com>
2026-02-17 10:44:21 +0100

    Create react router

--- aga-app/src/main.tsx
+++ aga-app/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  // { path: "/", element: <App /> },
+  { path: "/profile", element: <App /> }, // temporary, will be replaced with actual profile page
+  { path: "/signin", element: "" },
+  { path: "/signup", element: "" },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
