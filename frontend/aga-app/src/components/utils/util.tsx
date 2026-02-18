fc2a04d8c56906c7a310e128d8411b32cc8281a5
Arsenii Semenyako <cen9aoden@gmail.com>
2026-02-18 14:29:26 +0100

    Merge register and dev

---
+++ frontend/aga-app/src/components/utils/util.tsx
@@ -0,0 +1,9 @@
+export function validateEmail(email: string): boolean {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+export function validatePassword(password: string): boolean {
+  const passwordRegex = /^.{8,}$/;
+  return passwordRegex.test(password);
+}
--- frontend/aga-app/src/main.tsx
+++ frontend/aga-app/src/main.tsx
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.tsx";
+import Register from "./pages/Register.tsx";
 
 const router = createBrowserRouter([
   // { path: "/", element: <App /> },
   { path: "/profile", element: <App /> }, // temporary, will be replaced with actual profile page
   { path: "/signin", element: <LoginPage /> },
-  { path: "/signup", element: "" },
+  { path: "/signup", element: <Register /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
